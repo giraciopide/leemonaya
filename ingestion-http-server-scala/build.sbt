492295f008ea454ce7b59d0ccf1513f9862da3bb
Marco Nicolini <marco.nicolini@gmail.com>
2019-01-04 14:53:34 +0100

    Reworked imports

--- ingestion-http-server-scala/build.sbt
+++ ingestion-http-server-scala/build.sbt
@@ -1,6 +1,7 @@
 val Http4sVersion = "0.20.0-M4"
 val Specs2Version = "4.2.0"
 val LogbackVersion = "1.2.3"
+val CirceVersion = "0.10.1"
 
 lazy val root = (project in file("."))
   .settings(
@@ -9,10 +10,11 @@ lazy val root = (project in file("."))
     version := "0.0.1-SNAPSHOT",
     scalaVersion := "2.12.7",
     libraryDependencies ++= Seq(
+      "io.circe"        %% "circe-generic"       % CirceVersion,
       "org.http4s"      %% "http4s-blaze-server" % Http4sVersion,
       "org.http4s"      %% "http4s-circe"        % Http4sVersion,
       "org.http4s"      %% "http4s-dsl"          % Http4sVersion,
-      "org.specs2"     %% "specs2-core"          % Specs2Version % "test",
+      "org.specs2"      %% "specs2-core"          % Specs2Version % "test",
       "ch.qos.logback"  %  "logback-classic"     % LogbackVersion
     ),
     addCompilerPlugin("org.spire-math" %% "kind-projector"     % "0.9.6"),
--- ingestion-http-server-scala/src/main/scala/leemonaya/ServerRoutes.scala
+++ ingestion-http-server-scala/src/main/scala/leemonaya/ServerRoutes.scala
@@ -1,18 +1,33 @@
 package leemonaya
 
-import cats.effect.Sync
+import cats.effect.{IO, Sync}
 import io.circe.Json
 import org.http4s.HttpRoutes
-import org.http4s.circe.jsonEncoder
+import org.http4s.circe.{jsonEncoder, jsonOf}
 import org.http4s.dsl.Http4sDsl
+import io.circe.generic.auto._
+import org.http4s.circe.CirceEntityEncoder.circeEntityEncoder
 
 case class StationFeedMsg(stationId: String, temperature: Double, humidity: Double)
 
 class ServerRoutes[F[_]: Sync] extends Http4sDsl[F] {
+
+  implicit val stationFeedMsgJsonDecoder = jsonOf[F, StationFeedMsg]
+
   val routes: HttpRoutes[F] =
     HttpRoutes.of[F] {
       case GET -> Root / "hello" / name =>
-        Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+        Ok("ciao")
+      case POST -> Root / "station-feed-working" =>
+        //Ok(Json.obj("message" -> Json.fromString(s"Hello")))
+        NoContent()
+      case req @ POST -> Root / "station-feed" =>
+        Ok(req.as[StationFeedMsg])
+
+      case req @ POST -> Root / "station-feed2" =>
+        req.decode[StationFeedMsg] { m =>
+          Ok(m)
+        }
     }
 }
 
